866c5457a9583bedaaf61673011dee44f5f2d915
michele <michele.provolo@gmail.com>
2020-01-28 17:34:04 +0100

    create migration e model

---
+++ app/Employee.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Employee extends Model
+{
+    protected $fillable = [
+
+        'firstname',
+        'lastname'
+
+    ];
+
+    public function tasks() {
+        return $this -> belongsToMany(Task::class);
+    }
+}
---
+++ database/migrations/2020_01_28_160515_add_foreign_key.php
@@ -0,0 +1,48 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddForeignKey extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('employee_task', function (Blueprint $table) {
+
+            $table -> bigInteger('employee_id') -> unsigned() -> index();
+            $table -> foreign('employee_id', 'employee_task_employee_id')
+                   -> references('id')
+                   -> on('employees');
+
+            $table -> bigInteger('task_id') -> unsigned() -> index();
+            $table -> foreign('task_id', 'employee_task_task_id')
+                   -> references('id')
+                   -> on('tasks');
+
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('employee_task', function (Blueprint $table) {
+
+            $table -> dropForeign('employee_task_employee_id');
+            $table -> dropForeign('employee_task_task_id');
+
+            $table -> dropColumn('employee_id');
+            $table -> dropColumn('task_id');
+
+        });
+    }
+}
